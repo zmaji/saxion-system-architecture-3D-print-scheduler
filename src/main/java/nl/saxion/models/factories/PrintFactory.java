3ddb4ce842ac93cd367c47f302da06e744ed2b73
zmaji <502241@student.nl>
2021-12-06 18:06:52 +0100

    Cleaned PrintFactory class

--- src/main/java/nl/saxion/models/factories/PrintFactory.java
+++ src/main/java/nl/saxion/models/factories/PrintFactory.java
@@ -11,6 +11,15 @@ public class PrintFactory extends PrintSchedulerFactory {
         super(printerManager);
     }
 
+    /** Creates an instance of a Print based on given parameters from the Reader Class and adds it to a file managed by PrinterManager
+     *
+     * @param name the name of the Print
+     * @param filename the filename of the Print
+     * @param height the height of the Print
+     * @param width the width of the Print
+     * @param length the length of the Print
+     * @param filamentLength the filamentLength of the Print
+     */
     public void createPrint(String name, String filename, int height, int width, int length, ArrayList<Integer> filamentLength) {
         Print p = new Print(name, filename, height, width, length, filamentLength);
         printerManager.addToPrintList(p);
