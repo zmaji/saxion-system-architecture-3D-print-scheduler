ef775d9584bb8c9275d5aec21f4cc6a713a5b27e
zmaji <502241@student.nl>
2021-12-10 10:33:42 +0100

    Added HousedMultiColor class

---
+++ src/main/java/nl/saxion/models/printers/HousedMultiColor.java
@@ -0,0 +1,8 @@
+package nl.saxion.models.printers;
+
+public class HousedMultiColor extends MultiColor {
+
+    public HousedMultiColor(int id, String printerName, String manufacturer, int maxX, int maxY, int maxZ, int maxColors) {
+        super(id, printerName, manufacturer, maxX, maxY, maxZ, maxColors);
+    }
+}
