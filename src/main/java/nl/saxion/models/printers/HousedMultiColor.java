7b6e85a53c2460706c25bc4fd129933a7095f3f0
NKimenai <nilskimenai@hotmail.nl>
2022-01-20 11:52:57 +0100

    HousedMultiColor printerCompatibleWithTask behaviour adjusted

--- src/main/java/nl/saxion/models/printers/HousedMultiColor.java
+++ src/main/java/nl/saxion/models/printers/HousedMultiColor.java
@@ -1,8 +1,16 @@
 package nl.saxion.models.printers;
 
+import nl.saxion.models.prints.FilamentType;
+import nl.saxion.models.prints.PrintTask;
+
 public class HousedMultiColor extends MultiColor {
 
     public HousedMultiColor(int id, String printerName, String manufacturer, int maxX, int maxY, int maxZ, int maxColors) {
         super(id, printerName, manufacturer, maxX, maxY, maxZ, maxColors);
+    }
+
+    @Override
+    public boolean printerCompatibleWithTask(PrintTask printTask) {
+        return printTask.getFilamentType() == FilamentType.ABS && printTask.getColors().size() <= this.getMaxColors();
     }
 }
